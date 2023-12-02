5f6397d1ba55a7424427e0023d7c254a36d708a6
Jagadeesh5106 <141550989+Jagadeesh5106@users.noreply.github.com>
2023-12-02 12:42:02 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Jagadeesh');
+  if(req.url==='/home'){
+    res.write("Welcome to Home page");
+  }
+  else if(req.url==='/about'){
+    res.write("Welcome to about page");
+  }
+  else if(req.url==='/node'){
+    res.write("Welcome to Node js page");
+  }
+  res.end();
 });
 
-server.listen(4000, () => {
-  console.log('Server listening on port 4000');
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
